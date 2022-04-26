c02d6ef9690d45e904c6fe23b24b4174539616ba
Siavash Mirzaei-Fard <63229654+siavashmirzaeifard@users.noreply.github.com>
2022-04-26 23:10:18 +0430

    mixinClassesExplanationsWithSample
    there is lots of explanation and description top of classes that you should review them line by line to get the sample points.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,83 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+import 'package:meta/meta.dart' show useResult;
+
 import 'package:flutter/material.dart';
+import 'dart:developer' as devtools show log;
+
+extension Log on Object {
+  void log() => devtools.log(toString());
+}
+
+/* mixin can put constraint on their implementing classes to say for instance I'm a mixin and I need this property of this type. any object that uses me as a mixin have to have this property too. so it's kind
+of like abstract classes that they can put constraint on their implementing classes. */
+/* this mixin is not useful alone and should call in other classes to see how it works. */
+mixin CanRun {
+  int get speed;
+  void run() {
+    "Running at the speed of: $speed".log();
+  }
+}
+
+abstract class Animal {
+  const Animal();
+}
+
+mixin CanWalk on Animal {
+  int get speed;
+  void run() {
+    "Walking at the speed of: $speed".log();
+  }
+}
+
+/* cat class can inherit the mixin with "with" keyword.*/
+/* in OOP languages there is only one super class. however with mixin we can have multiple mixins into the same class using the "with" keyword. */
+class Cat with CanRun {
+  @override
+  int speed = 10;
+}
+
+/* the way we can inherit from both mixin and abstract class comes on the next line. because any class in Dart implicitly of the type Object even though you don't say extend object it is defaul of type object.
+so if we remove "extends Animal" the class dog can not work any more and we have an error which shows Dart is not happy about our implementation. */
+class Dog extends Animal with CanWalk {
+  @override
+  int speed = 5;
+}
+
+extension GetUri on Object {
+  Future<HttpClientResponse> getUrl(String url) =>
+      HttpClient().getUrl(Uri.parse(url)).then((req) => req.close());
+}
+
+/* this mixin is exactly like the CanRun mixin, CanRun gives an integer and return speed, here you will pass the URL String and get the result. */
+/* I prefer to only use the result of this particular class in mixin by adding "useResult" meta tag to use the result of the function */
+/* for this purpose I used "Live Server" extension for "VSCode" and instead of using live server load the names from the "api" folder which contains a json file as a hard coded json object. so press
+"cmd + shift + p" on mac or "ctrl + shift + p" on windows or linux and type live server and select on "open live server" and go to api/people.json, the url is your base url. (http://127.0.0.1:5500/api/people.json) */
+mixin CanMakeGetCall {
+  String get url;
+  @useResult
+  Future<String> getString() =>
+      getUrl(url).then((res) => res.transform(utf8.decoder).join());
+}
+
+@immutable
+class GetPeople with CanMakeGetCall {
+  const GetPeople();
+  @override
+  String get url => "http://127.0.0.1:5500/api/people.json";
+}
+
+/* getString method comes to people from the mixin class which we defined before. so we passed the url in People class and when we get an instance of People class we have access to mixin functions and properties
+and we're able to call getString method and parse our json file in utf8 format which we defined in our extension */
+void tester() async {
+  final cat = Cat();
+  cat.run();
+  cat.speed = 20;
+  cat.run();
+  final people = await const GetPeople().getString();
+  people.log();
+}
 
 void main() {
   runApp(const MyApp());
@@ -6,110 +85,25 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    tester();
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Home Page'),
+        centerTitle: true,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
